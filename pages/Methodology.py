498cbc3d4a723d58771b6de8782e1131f6157bc7
Jeff-Barlow-Spady <135166969+Jeff-Barlow-Spady@users.noreply.github.com>
2023-11-30 19:42:18 +0000

    Add methodology, data sources, and data preparation sections to the report

--- pages/Methodology.py
+++ pages/Methodology.py
@@ -3,20 +3,109 @@ import streamlit as st
 st.set_page_config(
     layout="wide", 
     page_title="Neighborhood Scoring System: Statistical Analysis and Model Development Report",
-    page_icon="📊"
+    page_icon="📊",
+    theme="auto"  # Set theme to "auto" to use system setting for light or dark mode
 )
 # Title
 st.title("Neighborhood Scoring System: Statistical Analysis and Model Development Report")
-
+st.markdown("""Analysis by Jeff Barlow-Spady""")    # Markdown text
+st.markdown("""This report details the methodology, analysis, and outcomes of the Neighborhood Scoring System project.""")
+st.divider()
+st.header("Objective")
+st.markdown(
+"""
+The primary goal of this analysis was to develop a comprehensive understanding of neighbourhood vulnerability using various socio-economic, 
+demographic, and environmental metrics. The aim was to distil these complex datasets into a single, 
+interpretable vulnerability score for each neighbourhood.
+""")
 # Executive Summary
 st.header("Executive Summary")
 st.markdown(
 """
-In this project, I embarked on a statistical journey to create a comprehensive scoring system for neighborhood evaluation. 
+In this project, I embarked on an ambitious project to create a comprehensive scoring system for neighborhood evaluation. 
 Through the application of factor analysis, machine learning optimization, and meticulous visual data exploration,
 I developed a model that not only accurately assesses neighborhood characteristics but also provides a statistically sound benchmark for comparison. 
 This report encapsulates the detailed analytical processes and presents the project's impactful outcomes.
 """)
+st.header("Data Sources")
+
+st.markdown("The analysis utilized three primary data sources:")
+
+st.markdown("### 2021 Federal Census - Long Form:")
+st.markdown("There were over 2000 characteristics measured for each neighbourhood. After reviewing the documentation with the dataset I chose the following possible indicators of exposure to social vulnerability:")
+st.markdown("- total_population")
+st.markdown("- homeowners_core_housing_need")
+st.markdown("- total_core_housing_need")
+st.markdown("- tennants_in_core_housing_need")
+st.markdown("- owners_30%*or_more")
+st.markdown("- total_30%plus_shelter")
+st.markdown("- tennants_30%plus_shelter_cost")
+st.markdown("- tennants_subs_housing")
+st.markdown("- Low_Income_Measure_18_to_64_years(%)")
+st.markdown("- Low_Income_Cutoff_18_to_64_years(%)")
+st.markdown("- housing_stock_1960_and_earlier")
+st.markdown("- housing_stock_61_to_80")
+st.markdown("- New LIM 65 years and over")
+st.markdown("- new LICO 65 plus")
+st.markdown("- proportion_children")
+st.markdown("- total sum Gini index on adjusted household after-tax income")
+st.markdown("- in_single_parent_families")
+st.markdown("- bottom_decile*%")
+st.markdown("- bottom_half_dist")
+st.markdown("- second_decile_prop")
+st.markdown("- Living alone 42 34")
+st.markdown("- less_than_hs")
+st.markdown("- non_perm_resident")
+st.markdown("- non_citizens")
+st.markdown("- unsuitable_housing")
+st.markdown("- Low-income cut-offs, after tax (LICO-AT) (%)")
+st.markdown("- Low-income measure, after tax (LIM-AT) (%)")
+st.markdown("- Renters")
+st.markdown("- visible_minority_population")
+st.markdown("- unemployed")
+
+st.markdown("### Edmonton Neighbourhoods 2021 Dataset:")
+st.markdown("From City of Edmonton Open Data Portal - used to match neighbourhood names, geometry and lat/long to the 2021 Federal Census.")
+
+st.markdown("### Shrubscriber Canopy Coverage vs Population Density:")
+st.markdown("I used this data to get average temperature, canopy information, confirmation of land area, and trees per square kilometre.")
+st.markdown("Key steps in data preparation included merging these datasets on neighborhood names, normalizing the data, and calculating additional metrics such as population density. Used to create:")
+st.markdown("- Lacking_canopy - the compliment of the land area per neighbourhood without measured canopy")
+st.markdown("- Population Density (People/sq. km)")
+
+st.header("Data Preparation")
+
+st.markdown("All data was cleaned, validated and then combined into a single dataset using Excel and Python.")
+
+st.markdown("All numeric columns were converted to proportions of the population, made uni-directional so that a higher number denotes increased risk of vulnerability.")
+
+st.markdown("Data Normalization: For this analysis I used Z-Score Normalization. Z-score normalization is a statistical technique used to standardize data by transforming it to a common scale. It calculates the z-score for each data point, which represents the number of standard deviations that data point is away from the mean. This normalization process allows for easier comparison and interpretation of data across different variables or datasets.")
+
+st.markdown("Building score categories was the next step. I used the method similar to the one described in this paper:")
+st.markdown("Reid, C., O’Neill, M., Gronlund, C., Brines, S., Brown, D., Diez-Roux, A., Schwartz, J. (2009). Mapping community determinants of heat vulnerability. Environmental Health Perspectives, 117(11), 1730–1736.")
+st.markdown("[http://doi.org/10.1289/ehp.0900683](http://doi.org/10.1289/ehp.0900683)")
+st.code(
+"""
+#Calculate 'VI' columns/Index scores based on the method from Reid et all 2009
+def calculate_index(x):
+    if x <= -2:
+        return 1
+    elif -2 < x <= -1:
+        return 2
+    elif -1 < x <= 0:
+        return 3
+    elif 0 < x <= 1:
+        return 4
+    elif 1 < x <= 2:
+        return 5
+    elif x > 2:
+        return 6
+    else:
+        return 7
+#insert into current dataframe
+for column in ind_columns:
+    subset_df[column + '_VI'] = subset_df[column].apply(calculate_index)
+""", language="python")
 
 # Factor Analysis
 st.header("Factor Analysis")
